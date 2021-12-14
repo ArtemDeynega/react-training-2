90c5085f9c4893432712272c1a1162161d65b7c4
artem <artemdeynega892@gmail.com>
2021-12-14 11:57:37 +0200

    модуль 1 занятие 1 компоненты и пркатика 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import PaintingList from './components/PaintingList';
+// import PaintingList from './components/PaintingList';
+import FirstComponents from 'components/FirstComponents/FirstComponents';
 
 // import paintings from './paintings.json';
 // import Section from 'components/Section';
 // import colorPikerOptions from './components/colorPikerOptions.json';
 // import ColorPiker from 'components/ColorPiker';
-import Alert from 'components/Alert';
+// import Alert from 'components/Alert';
 
 export default function App() {
   return (
     <div>
+      <FirstComponents number={37}>See you to Thursdey</FirstComponents>
+      <FirstComponents number={41}>See you in 2021</FirstComponents>
       {/* <ColorPiker options={colorPikerOptions} />
       <PaintingList items={paintings} /> */}
 
-      <Alert text="Шеф все пропало" type="success" />
+      {/* <Alert text="Шеф все пропало" type="success" />
       <Alert text="Шеф все пропало" type="warning" />
-      <Alert text="Шеф все пропало" type="error" />
+      <Alert text="Шеф все пропало" type="error" /> */}
       {/* <Section title="Топ недели">
         <PaintingList items={paintings} />
       </Section>
